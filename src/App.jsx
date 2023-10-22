574b4f7923535ab34e70de99acb793e495fc6cff
Nivash23 <nivashsubash32@gmail.com>
2023-10-22 12:50:42 +0530

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,21 @@ function App() {
     let user = document.getElementById('username');
   let descript = document.getElementById('description');
   let status = document.getElementById('status');
+  let edt = document.getElementById('edit');
+  let del = document.getElementById('delete');
+  let card = document.getElementById('cards');
+  let databs = document.getElementById('database');
     if (data)
     {
       user.textContent = `Name:${name}`;
       descript.textContent = `Description:${des}`  
-      status.style.display='inline';
+      status.style.display = 'inline';
+      del.style.display = 'inline';
+      edt.style.display = 'inline';
+      
+
+
+      setData(false);
     }
   // let name = document.getElementById('name');
   // let description = document.getElementById('descript');
@@ -42,17 +52,19 @@ function App() {
         <button type="submit" onClick={()=>setData(true)}>Add Todo</button>
       </div>
      <div id='datas'>
-        <h3>My todos</h3>
-        <h3>Status :<span class='dropdown-toggle' data-bs-toggle='dropdown'>All
+        <h4>My todos</h4>
+        <h4>Status :<span class='dropdown-toggle' data-bs-toggle='dropdown'>All
         </span>
         <ul class='dropdown-menu'>
           <li> <a class='dropdown-item' href='#'>completed</a></li>
           <li> <a class='dropdown-item' href='#'>Not completed</a></li>
           
           </ul>
-          </h3>
-      </div>
-      <div id='database'>
+        </h4>
+        <ul id='cards'>
+          <li>
+
+        <div id='database'>
          <p id='username'></p>
         <p id='description'></p>
         <p id='status'>status:<span  class='dropdown-toggle' data-bs-toggle='dropdown'></span>
@@ -60,9 +72,16 @@ function App() {
             <li> <a class='dropdown-item' href='#'></a>completed</li>
             <li> <a class='dropdown-item' href='#'></a>Not completed</li>
           </ul></p>
+        <div id='buttons'>
+          <button id='edit'  type='button'>Edit</button>
+          <button id='delete' type='button'>Delete</button>
+        </div>
       </div>
-
+          </li>
+        </ul>
+      </div>
     </div>
+
   )
 }
 
